c595623a7abb0b1f0bf3e072b938c41e4030f624
Geraskin Denis <fresh7tea@gmail.com>
2022-08-10 13:25:03 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Geraskin Denis");
+        System.out.println("04.05.1980");
+    }
+}
