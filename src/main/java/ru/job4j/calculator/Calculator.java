6e122f9c2eb0a31a85023e76957741a6bf50ad0d
Geraskin Denis <fresh7tea@gmail.com>
2022-08-11 20:47:02 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,11 +1,16 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        int result = one + two;
+        System.out.println(result);
+
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);
 
         int six = 6;
         int four = 4;
